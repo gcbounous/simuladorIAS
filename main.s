f0f5df28be29a771e3714dc13872c67b6fab4929
Gabriel Caê Bounous <gccbounous@gmail.com>
2015-11-21 13:13:12 -0200

    loop funcionando

--- main.s
+++ main.s
@@ -1,43 +1,63 @@
-	.globl main
-	.extern MMAP
-	.extern AC
-	.extern PC
-	.extern MQ
+.globl main
+.extern MMAP
+.extern AC
+.extern PC
+.extern MQ
 
-	.data
-p_linha:	 .asciz "%03X %02X %03X %02X %03X\n"
-p_inicial:	 .asciz "@ Estado inicial:\n"
-p_estado:	 .asciz "+ AC:  0x%03X     MQ: 0x%03X        PC: 0x%03X\n"
+.data
+
+p_linha:		.asciz "%03X %02X %03X %02X %03X\n"
+p_inicial:		.asciz "@ Estado inicial:\n"
+p_estado:		.asciz "+ AC:  0x%010llX     MQ: 0x%010llX        PC: 0x%0010llX\n--------------------------------------------------------------\n"
+p_execucao:		.asciz "@ Executando instrucao no endereco %010X "
+p_execucao_esquerda:	.asciz "(instrucao a esquerda)\n"
+p_execucao_direita:		.asciz "(instrucao a direita)\n"
 mask2: .word 0xFF
 mask3: .word 0xFFF
+a_direita: .word 0x0
 
-	.text
-	.align 4
+.text
+.align 4
 
 main:
 	push {ip, lr}
 
-	@ Linha do mapa de memoria a ser impressa
 	bl inicializacao
+	@ TODO: verificar todos os endereços!
 	bl simulacao
-	@bl leitura_linha
 	bl exit
 
 simulacao:
 	push {ip, lr}
 	mov r1, #0
 	loop_mmap:
-		bl leitura_linha
-		cmp r2, #0	@ verifica se os opcode1 é 0
-		beq exit
-		cmp r4, #0 @ erifica se os opcode2 é 0
-		beq exit
-		@TODO: aqui fazer switch!!
-		bl impressao_linha
-		add r1, r1, #1
-		b loop_mmap
+		ldr r0, =a_direita
+		ldr r0, [r0]
+		cmp r0, #0
+		beq pegar_esquerda
+		b pegar_direita
+		pegar_direita: @ instrucao a direita
+			mov r2, r4	@ r2 <- opcode
+			mov r3, r5	@ r3 <- end
+			b saida_if
 
-	pop {ip, pc}
+		pegar_esquerda: @instrucao a esquerda
+			add r1, r1, #1 @proxima linha
+			bl leitura_linha
+			b saida_if
+
+		saida_if:
+			cmp r2, #0	@ verifica se os opcode1 é 0, indica o final do programma
+			beq exit
+			bl impressao_execucao
+			bl impressao_estado
+			@ TODO: aqui fazer switch!!
+			bl impressao_linha
+			ldr r6, =a_direita
+			ldr r0, [r6]
+			eor r0, r0, #1 @ toggle a_direita
+			str r0, [r6]
+			b loop_mmap
 	
 leitura_linha:	
 	push {ip, lr}
@@ -94,11 +114,11 @@ inicializacao:
 	@ impressao inicial
 	ldr r0, =p_inicial
 	bl printf
-	bl impressao_variaveis
+	bl impressao_estado
 
 	pop {ip, pc}
 
-impressao_variaveis:
+impressao_estado:
 	push {r1-r5, ip, lr}
 
 	ldr r0, =p_estado
@@ -119,9 +139,36 @@ impressao_linha:
 	ldr r0, =p_linha	
 	push {r4, r5}
 	bl printf
-	pop {r14, r5}
+	pop {r4, r5}
 
 	pop {r1-r5, ip, pc}
+
+impressao_execucao:
+	push {r1-r5, ip, lr}
+
+	ldr r0, =p_execucao	
+	bl printf
+	ldr r6, =a_direita
+	ldr r6, [r6]
+	cmp r6, #0
+	beq impressao_esquerda
+	cmp r6, #1
+	beq impressao_direita
+	b sair_impressao_execucao
+
+	impressao_esquerda:
+		ldr r0, =p_execucao_esquerda	
+		bl printf
+		b sair_impressao_execucao
+
+	impressao_direita:
+		ldr r0, =p_execucao_direita	
+		bl printf
+		b sair_impressao_execucao
+
+	sair_impressao_execucao:
+		pop {r1-r5, ip, pc}
+
 
 exit:
 	pop { ip, pc}
